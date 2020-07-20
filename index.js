8451af7f23d7fb4009f5d6dc5cd4e9487d1499d8
Okechukwu Ezeobele <chimaokeyjay@gmail.com>
2020-07-20 13:12:08 +0100

    commit on master

--- index.js
+++ index.js
@@ -91,12 +91,63 @@ app.post('/register', ifLoggedin,
             return error.msg;
         });
         // REDERING login-register PAGE WITH VALIDATION ERRORS
-        res.render('login-register',{
+        res.json({
             register_error:allErrors,
             old_data:req.body
         });
     }
 });// END OF REGISTER PAGE
+
+// // REGISTER PAGE 2
+// app.post('/register2', ifLoggedin, 
+// // post data validation(using express-validator)
+// [
+//     body('user_email','Invalid email address!').isEmail().custom((value) => {
+//         return dbConnection.execute('SELECT `email` FROM `users` WHERE `email`=?', [value])
+//         .then(([rows]) => {
+//             if(rows.length > 0){
+//                 return Promise.reject('This E-mail already in use!');
+//             }
+//             return true;
+//         });
+//     }),
+//     body('user_name','Username is Empty!').trim().not().isEmpty(),
+//     body('user_pass','The password must be of minimum length 6 characters').trim().isLength({ min: 6 }),
+// ],// end of post data validation
+// (req,res,next) => {
+
+//     const validation_result = validationResult(req);
+//     const {user_name, user_pass, user_email} = req.body;
+//     // IF validation_result HAS NO ERROR
+//     if(validation_result.isEmpty()){
+//         // password encryption (using bcryptjs)
+//         bcrypt.hash(user_pass, 12).then((hash_pass) => {
+//             // INSERTING USER INTO DATABASE
+//             dbConnection.execute("INSERT INTO `users`(`name`,`email`,`password`) VALUES(?,?,?)",[user_name,user_email, hash_pass])
+//             .then(result => {
+//                 res.send(`your account has been created successfully, Now you can <a href="/">Login</a>`);
+//             }).catch(err => {
+//                 // THROW INSERTING USER ERROR'S
+//                 if (err) throw err;
+//             });
+//         })
+//         .catch(err => {
+//             // THROW HASING ERROR'S
+//             if (err) throw err;
+//         })
+//     }
+//     else{
+//         // COLLECT ALL THE VALIDATION ERRORS
+//         let allErrors = validation_result.errors.map((error) => {
+//             return error.msg;
+//         });
+//         // REDERING login-register PAGE WITH VALIDATION ERRORS
+//         res.render('login-register',{
+//             register_error:allErrors,
+//             old_data:req.body
+//         });
+//     }
+// });// END OF REGISTER PAGE
 
 // LOGIN PAGE
 app.post('/', ifLoggedin, [
@@ -127,7 +178,7 @@ app.post('/', ifLoggedin, [
                     res.redirect('/');
                 }
                 else{
-                    res.render('login-register',{
+                    res.json({
                         login_errors:['Invalid Password!']
                     });
                 }
@@ -146,7 +197,7 @@ app.post('/', ifLoggedin, [
             return error.msg;
         });
         // REDERING login-register PAGE WITH LOGIN VALIDATION ERRORS
-        res.render('login-register',{
+        res.json({
             login_errors:allErrors
         });
     }
